1e05b672f3bf05929d5a1e45e7e001b51e8b41f5
dlee <dlee@touchcommerce.com>
2016-04-15 11:37:57 -0700

    JAVA ID readAllLine example has been added

---
+++ learning-java/learning.java.lang/src/main/java/cwl/essencial/io/ReadReadAllLinesExample.java
@@ -0,0 +1,47 @@
+package cwl.essencial.io;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * http://www.java2s.com/Tutorials/Java/java.nio.file/Files/
+ * Java_Files_readAllLines_Path_path_Charset_cs_.htm
+ *
+ */
+public class ReadReadAllLinesExample {
+
+	public static void main(String[] args) {
+
+		Path wiki_path = Paths.get("C:/tutorial/wiki", "wiki.txt");
+
+		Charset charset = Charset.forName("ISO-8859-1");
+		try {
+			List<String> lines = Files.readAllLines(wiki_path, charset);
+
+			for (String line : lines) {
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+
+	}
+
+	public static void readAllBytes() {
+		Path wiki_path = Paths.get("C:/tutorial/wiki", "wiki.txt");
+
+		try {
+			byte[] wikiArray = Files.readAllBytes(wiki_path);
+
+			String wikiString = new String(wikiArray, "ISO-8859-1");
+			System.out.println(wikiString);
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+
+	}
+}
