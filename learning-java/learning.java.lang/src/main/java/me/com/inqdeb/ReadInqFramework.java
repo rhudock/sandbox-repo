fa2d7edcc353ebaef31c523f82c1cb15ef1436c5
dlee <dlee@touchcommerce.com>
2016-06-22 00:22:22 -0700

    Add some projects.

--- learning-java/learning.java.lang/src/main/java/me/com/inqdeb/ReadInqFramework.java
+++ learning-java/learning.java.lang/src/main/java/me/com/inqdeb/ReadInqFramework.java
@@ -1,11 +1,14 @@
-package cwl.essencial.io;
+package me.com.inqdeb;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-public class ReadFileStreamExample {
+/**
+
+ */
+public class ReadInqFramework {
 
     public static void main(String args[]) {
 
