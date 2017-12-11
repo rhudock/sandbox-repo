92eed32a78e75830fc60a36aa106cd8c47e6416f
dlee <dlee@touchcommerce.com>
2017-12-11 10:15:34 -0800

    Java Security Unit Test

--- learning-java/learning.java.lang/src/test/java/cwl/security/nina/CBASignatureValidaterTest.java
+++ learning-java/learning.java.lang/src/test/java/cwl/security/nina/CBASignatureValidaterTest.java
@@ -11,6 +11,7 @@ import static org.junit.Assert.*;
 public class CBASignatureValidaterTest {
     private static final Logger logger = LoggerFactory.getLogger(CBASignatureValidaterTest.class);
     private static final String plainText = "{\"TalkAgentRequest\":{\"@SCI\":\"\",\"@IID\":\"userIdentifier\",\"@TimeStamp\":\"2017-11-01T14:48:26.942763+10:00\",\"UserText\":\"userText\",\"NleResults\":true,\"NinaVars\":{\"assetType\":\"assetType\",\"invocationpoint\":\"invocationPoint\"}}}";
+    private static final String plainTextMod = "{\"TalkAgentRequest\":{\"@SCI\":\"test\",\"@IID\":\"userIdentifier\",\"@TimeStamp\":\"2017-11-01T14:48:26.942763+10:00\",\"UserText\":\"userText\",\"NleResults\":true,\"NinaVars\":{\"assetType\":\"assetType\",\"invocationpoint\":\"invocationPoint\"}}}";
 
     @Test
     public void getSignatureStrAndgetEncryptedTest() throws Exception {
@@ -19,7 +20,7 @@ public class CBASignatureValidaterTest {
     }
 
     @Test
-    public void verifySigTest() throws Exception {
+    public void verifySigTestSuccess() throws Exception {
         byte[] signitureStr = CBASignatureValidater.getSignature(plainText, null);
         logger.debug("signatureStr : {}", signitureStr);
 
@@ -28,5 +29,16 @@ public class CBASignatureValidaterTest {
         sig.update(plainText.getBytes());
 
         assertTrue(sig.verify(signitureStr));
+    }
+    @Test
+    public void verifySigTestFail() throws Exception {
+        byte[] signitureStr = CBASignatureValidater.getSignature(plainText, null);
+        logger.debug("signatureStr : {}", signitureStr);
+
+        Signature sig = Signature.getInstance("SHA256withRSA");
+        sig.initVerify(CBASignatureValidater.getPublicKey(null));
+        sig.update(plainTextMod.getBytes());
+
+        assertTrue(!sig.verify(signitureStr));
     }
 }
