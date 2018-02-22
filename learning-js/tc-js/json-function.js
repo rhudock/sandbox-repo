dee588d836cd1b4e55cb0ff178a9a2a6c43c3caf
dlee <dlee@touchcommerce.com>
2018-02-21 23:01:32 -0800

    Start learning-js

---
+++ learning-js/tc-js/json-function.js
@@ -0,0 +1,34 @@
+/*
+
+*/
+
+let testStr = '[' +
+    '[ { "funName": "fname1", "arguments": [] } ],' + 
+    '[ { "funName": "fname1", "arguments": [] }, { "funName": "fname1", "arguments": [] } ]' +
+    ']';
+
+console.log(typeof testStr);
+
+let testJson = JSON.parse(testStr);    
+
+let liveChatMock = {
+    fname1: function() {
+        return this;
+    },
+    toString: function() {
+        return "I am liveChatMock";
+    }
+}
+
+console.log("-----------------------");
+let i = 0;
+testJson.forEach(element => {
+    i++;
+    var rootObj = liveChatMock;
+    element.forEach(liveFunElement => {
+        rootObj = liveChatMock[liveFunElement.funName].apply(rootObj, liveFunElement.arguments);
+    });
+});
+
+
+process.exit(1)
