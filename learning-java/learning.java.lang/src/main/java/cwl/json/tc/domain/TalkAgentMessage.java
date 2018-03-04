2792a7b3e29a5681da1654c41cd6b58ce80dc618
chealwoo <Just1Do>
2018-03-03 16:25:06 -0800

    jackson tutorial start.

---
+++ learning-java/learning.java.lang/src/main/java/cwl/json/tc/domain/TalkAgentMessage.java
@@ -0,0 +1,17 @@
+package cwl.json.tc.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class TalkAgentMessage {
+    private TalkAgentResponse talkAgentResponse;
+
+    @JsonCreator
+    public TalkAgentMessage(@JsonProperty("TalkAgentResponse") TalkAgentResponse talkAgentResponse) {
+        this.talkAgentResponse = talkAgentResponse;
+    }
+
+    public TalkAgentResponse getTalkAgentResponse() {
+        return talkAgentResponse;
+    }
+}
