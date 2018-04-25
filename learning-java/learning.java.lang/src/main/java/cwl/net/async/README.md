39616ab974b066cb80f6578e713a18632fd4bb2a
chealwoo <Just1Do>
2018-04-25 00:46:08 -0700

    AsyncRestTemplate

---
+++ learning-java/learning.java.lang/src/main/java/cwl/net/async/README.md
@@ -0,0 +1,4 @@
+* https://github.com/waichee/spring4-rest-client-example
+
+
+* https://dzone.com/articles/getting-started-with-api-functional-testing-with-b
---
+++ learning-java/learning.java.lang/src/main/java/cwl/net/async/response/Error.java
@@ -0,0 +1,32 @@
+package cwl.net.async.response;
+
+public class Error {
+
+    String type;
+
+    String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Error{" +
+                "description='" + description + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+}
