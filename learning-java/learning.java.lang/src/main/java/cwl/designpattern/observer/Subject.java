5fff2488394186e0e8af5c4edf5f446b7a82a76d
dlee <daniel.lee@nuance.com>
2018-05-21 00:18:24 -0700

    Design Pattern - Observer

---
+++ learning-java/learning.java.lang/src/main/java/cwl/designpattern/observer/Subject.java
@@ -0,0 +1,16 @@
+package cwl.designpattern.observer;
+
+public interface Subject {
+
+    //methods to register and unregister observers
+    void register(Observer obj);
+
+    void unregister(Observer obj);
+
+    //method to notify observers of change
+    void notifyObservers();
+
+    //method to get updates from subject
+    Object getUpdate(Observer obj);
+
+}
