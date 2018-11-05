5de929cc55d9ec8da306b5260ba9bd633dcebd8f
dlee <daniel.lee@nuance.com>
2018-11-04 22:08:16 -0800

    CSP Security iFrame test

--- learning-js/node-simple/csptest-v2.js
+++ learning-js/node-simple/csptest-v2.js
@@ -23,8 +23,35 @@ http.createServer(function (req, res) {
                 res.writeHead(200, {
                     'Content-Type': 'text/html',
                     // 'X-Frame-Options': "SAMEORIGIN",
+                   'Content-Security-Policy': "frame-ancestors 'self' http://dleetest:3000",    // -- Working main page 'http://dleetest:3000/index.html'
+                   // 'Content-Security-Policy': "frame-ancestors 'self' http://*.dleetest:3000",    // -- NOT Working main page 'http://dleetest:3000/index.html'
+                   // 'Content-Security-Policy': "frame-ancestors http://*.dleetest:3000",    // -- NOT Working main page 'http://dleetest:3000/index.html'
                     // 'Content-Security-Policy': "frame-ancestors http://localhost:3000 'self'",
+                   // 'Content-Security-Policy': "frame-ancestors 'self' http://dleemac:3000",
+                    'location': 'myTest'
+                });
+                res.write(data);
+                res.end();
+            });
+            break;
+        case '/iframe-index-sameorigin.html':
+            fs.readFile('iframe-index.html', function (err, data) {
+                res.writeHead(200, {
+                    'Content-Type': 'text/html',
+                    'X-Frame-Options': "SAMEORIGIN",
                     'Content-Security-Policy': "frame-ancestors 'self' http://dleetest:3000",
+                    'location': 'myTest'
+                });
+                res.write(data);
+                res.end();
+            });
+            break;
+        case '/iframe-index-no-ancestor.html':
+            fs.readFile('iframe-index.html', function (err, data) {
+                res.writeHead(200, {
+                    'Content-Type': 'text/html',
+                    // 'X-Frame-Options': "SAMEORIGIN",
+                    'Content-Security-Policy': "frame-ancestors 'self' http://dleetest",
                     'location': 'myTest'
                 });
                 res.write(data);
